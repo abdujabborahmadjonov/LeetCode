72a34a3a462285bd5a0bf1fac8b297ad79e532ab
abdujabborahmadjonov <ghub9472@gmail.com>
2026-07-01 15:31:57 -0600

    e

---
+++ part 1/Sort_merge.py
@@ -0,0 +1,32 @@
+from multiprocessing.managers import Array
+
+numbers = [1,3,4,5,2,16,7,2,6,7,2,3,5,23,5,6,23,33,11,5,6]
+
+
+
+
+def mergeSort(arr):
+    if len(arr)<=1:
+        return arr
+    mid = len(arr)//2
+    left = mergeSort(arr[:mid])
+    right = mergeSort(arr[mid:])
+    return merge(left, right)
+
+def merge(left, right):
+    result = []
+    i = 0
+    j = 0
+
+    while i<len(left) and j<len(right):
+        if left[i]<right[j]:
+            result.append(left[i])
+            i+=1
+        else:
+            result.append(right[j])
+            j+=1
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
+
+print(mergeSort(numbers))
