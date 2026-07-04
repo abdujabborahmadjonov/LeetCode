45659bcd890a3761e75f04da8f98b0c1a56b4b61
abdujabborahmadjonov <ghub9472@gmail.com>
2026-07-04 01:44:24 -0600

    Auto save Sat Jul  4 01:44:24 MDT 2026

--- easy/HighestAltitude.py
+++ easy/HighestAltitude.py
@@ -5,6 +5,6 @@ class Solution:
 
         for g in gain:
             altitude += g
-            highest = max(highest, altitude)  ddkk
+            highest = max(highest, altitude)  ddkkkk
 
         return highest
