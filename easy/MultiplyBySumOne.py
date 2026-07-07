e9d6841c2f5532ec25318dc0abf5fe2bc8410695
abdujabborahmadjonov <ghub9472@gmail.com>
2026-07-06 23:25:32 -0600

    e

---
+++ easy/MultiplyBySumOne.py
@@ -0,0 +1,18 @@
+from unittest.mock import sentinel
+
+
+def sumAndMultiply( n: int) -> int:
+    a = str(n)
+    b=""
+    c=0
+    for e in a:
+        if e != '0':
+            b+=e
+            c+=int(e)
+    return int(b)*c
+
+
+
+
+
+
