5c55627bce579653f631e9b2d3ee2a1c69693233
abdujabborahmadjonov <ghub9472@gmail.com>
2026-07-12 14:01:24 -0700

    e

---
+++ easy/RankTransformOfAnArray.py
@@ -0,0 +1,17 @@
+from typing import List
+
+def arrayRankTransform(arr: List[int]) -> List[int]:
+
+    sorted_unique = sorted(set(arr))
+
+
+    rank = {}
+    for i, num in enumerate(sorted_unique, start=1):
+        rank[num] = i
+
+
+    return [rank[num] for num in arr]
+
+
+arr = [40, 10, 20, 30]
+print(arrayRankTransform(arr))
